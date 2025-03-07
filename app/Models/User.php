3c8f90d8f703c403f2c714184036eb8d549ad303
Hessam Taghvaei <mohaphez@yahoo.com>
2025-03-07 12:41:46 +0330

    feat: Add Bookmark model with user relationship and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,7 +12,6 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
 
     protected $fillable = [
         'name',
@@ -30,5 +30,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function bookmarks(): HasMany
+    {
+        return $this->hasMany(Bookmark::class);
     }
 }
---
+++ database/migrations/2025_03_07_088571_create_bookmarks_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('url');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+
+
+            $table->unique(['user_id', 'url']);
+            $table->index('title');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+};
